995f04d993fec73494b522c91d99fe1a248b6fbd
Robert <robert.kosak237@gmail.com>
2017-08-10 18:46:23 +0200

    javascript

--- js/app.js
+++ js/app.js
@@ -1,1 +1,20 @@
-$(document).foundation();
+$(document).foundation({
+  const button = document.querySelector(".submit");
+    const loginInput = document.querySelector(".login");
+
+    button.addEventListener("click", event => {
+      
+      event.preventDefault();
+      console.log(loginInput.value);
+
+      if (loginInput.value == 0)  {
+      loginInput.classList.add("alert");
+
+    } else {
+      loginInput.classList.remove("alert");
+    }
+  }
+    );
+
+
+});

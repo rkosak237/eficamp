5c138fece3d452b9bf5226656487ab3a32c3e6fb
Robert <robert.kosak237@gmail.com>
2017-08-12 20:55:56 +0200

    cleaning divs in html

--- js/app.js
+++ js/app.js
@@ -1,20 +1,46 @@
-$(document).foundation({
+$(document).foundation();
+$(document).ready(function() {
   const button = document.querySelector(".submit");
-    const loginInput = document.querySelector(".login");
+  const passInput = document.querySelector(".pass");
 
-    button.addEventListener("click", event => {
-      
-      event.preventDefault();
-      console.log(loginInput.value);
+  button.addEventListener("click", event => {
 
-      if (loginInput.value == 0)  {
-      loginInput.classList.add("alert");
+    event.preventDefault();
+    console.log(passInput.value);
+
+    if (passInput.value == 0) {
+      passInput.classList.add("alert");
+      alert('use proper login');
+
 
     } else {
-      loginInput.classList.remove("alert");
+      passInput.classList.remove("alert");
+      let yourLogin = passInput.value;
+      let yourPass =
+      $.ajax({
+        type: "post",
+        data: {
+          login: showLog.value,
+          password: passInput.value
+        },
+        url: "https://efigence-camp.herokuapp.com/api/login",
+        error: function(response) {
+          console.log(response.responseText);
+        },
+        success: function(response) {
+          console.log(response);
+        }
+      });
     }
-  }
-    );
+  });
+  const changeBtn = document.querySelector(".btnlog");
+  const showLog = document.querySelector(".custominput");
 
+
+  changeBtn.addEventListener("click", function() {
+    changeBtn.classList.toggle("hide");
+    showLog.classList.toggle("hide");
+  });
 
 });
+//dodac do pola trojkat ktory bedzie wskazywal bledne pole, znajduje się na inpucie ::after

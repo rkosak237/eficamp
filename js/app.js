1aa02824ebc50eb32060419f82be6077b4ae70a5
Robert <robert.kosak237@gmail.com>
2017-08-20 23:20:28 +0200

    turning on grids, fixing layout, adding home-layout

--- js/app.js
+++ js/app.js
@@ -1,14 +1,14 @@
 $(document).foundation();
+// Login-page
 $(document).ready(function() {
   const button = document.querySelector(".submit");
   const passInput = document.querySelector(".pass");
   const changeBtn = document.querySelector(".btnChangeLogin");
   const showLog = document.querySelector(".login");
-  const hideLogExample = document.querySelector(".logToInput")
+  const hideLogExample = document.querySelector(".logToInput");
   const addArrow = document.querySelector(".wrap");
 
   button.addEventListener("click", event => {
-
     event.preventDefault();
     console.log(passInput.value);
 
@@ -47,10 +47,13 @@ $(document).ready(function() {
     }
   });
 
-  changeBtn.addEventListener("click", function() {
+    changeBtn.addEventListener("click", function() {
     changeBtn.classList.toggle("hide");
     showLog.classList.toggle("hide");
     hideLogExample.classList.toggle("hide");
   });
+
+// Home page
+
 
 });
